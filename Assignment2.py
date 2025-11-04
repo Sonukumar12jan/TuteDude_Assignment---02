69fe9e261aa98308edf5f045ed578e253cd31e7d
sonu kumar <163671481+Sonukumar12jan@users.noreply.github.com>
2025-11-04 22:19:38 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,15 @@
+# Task - 1
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
+    
+print()  # Just to add a blank line between tasks
+    
+# Task - 2
+total = 0
+for i in range(1, 51):
+    total += i
+print("The sum of numbers from 1 to 50 is:", total)
